bc9a4d0bae743a617bc2b747b97796dfe4c169ed
tatianacartagena <142192156+tatianacartagena@users.noreply.github.com>
2025-04-08 19:11:02 -0500

    Delete ProyectoFinal2025-1/ProyectoFinalEstructura directory

--- ProyectoFinal2025-1/ProyectoFinalEstructura/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module co.edu.uniquindio.proyectofinalestructura.proyectofinalestructura {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens co.edu.uniquindio.proyectofinalestructura.proyectofinalestructura to javafx.fxml;
-    exports co.edu.uniquindio.proyectofinalestructura.proyectofinalestructura;
-}
